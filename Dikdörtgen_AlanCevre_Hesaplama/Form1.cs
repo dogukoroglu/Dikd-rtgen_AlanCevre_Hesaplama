0f4226d2ede42b4d2704f6de50d38c0b0f44f659
korog <korog@LAPTOP-GPMKKC5N>
2022-07-03 11:28:05 +0300

    Proje dosyası ekle.

---
+++ Dikdörtgen_AlanCevre_Hesaplama/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dikdörtgen_AlanCevre_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int kkenar = Convert.ToInt16(textBox1.Text);
+            int ukenar = Convert.ToInt16(textBox2.Text);
+            int alan = kkenar * ukenar;
+            int cevre = (kkenar + ukenar) * 2;
+                
+            label4.Text = alan.ToString();
+            label5.Text = cevre.ToString();
+        }
+    }
+}
